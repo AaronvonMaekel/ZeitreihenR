49ddd27b05e2da9a7c8810a91007b24688d0265d
Aaron Mäkel <cv234@stud.uni-heidelberg.de>
2024-08-01 15:34:12 +0200

    added validity checks to classes

--- classes.R
+++ classes.R
@@ -1,7 +1,8 @@
 
-
 # Laden der erforderlichen Bibliothek
 library(methods)
+
+#S4-Klassendefinitionen
 setClass(
     "TimeSeries",
     slots = list(
@@ -14,7 +15,7 @@ setClass(
     
     
 )
-# Definition der S4-Klasse für AR(p) Zeitreihe
+
 setClass(
     "AR",
     slots = list(
@@ -26,7 +27,17 @@ setClass(
     
 )
 
-# Benutzerdefinierter Konstruktor, nur benötigt wenn new nicht ausreicht
+setClass(
+    "MA",
+    slots = list(
+        ma_params = "numeric"
+    ),
+    contains="TimeSeries",
+    prototype = list(ma_params = NA_real_)
+    
+)
+
+# Benutzerdefinierte Konstruktoren
 AR <- function(ar_params = NA_real_,start_values=NA_real_,n=1,sd=1) {
     
     p <- length(ar_params)
@@ -57,20 +68,89 @@ AR <- function(ar_params = NA_real_,start_values=NA_real_,n=1,sd=1) {
         data= time_series)
 }
 
+MA <- function(ma_params = NA_real_,sd=1,n=1) {
+    
+    ma_params <- ma_params
+    q <- length(ma_params)
+    
+    # Initialisieren der Zeitreihe mit Nullen
+    time_series <- numeric(n)
+    
+    # White-Noise-Komponente generieren
+    noise <- rnorm(n+q , 0, sd)
+    
+    
+    # Generieren der MA(q)-Zeitreihe
+    for (t in (q + 1):(n + q)) {
+        time_series[t] <- sum(ma_params * rev(time_series[(t-q):(t-1)])) + noise[t]
+    }
+    
+    new("MA",
+        ma_params=ma_params,
+        sd=sd,
+        data=time_series,
+        n=n)
+}
+
+#Validierungen
 setValidity("TimeSeries", function(object){
-    TRUE #TODO laenge check und sd check
+    if (!is.atomic(object@n)){
+        "length is not atomic"
+    }
+    else if (0>object@n){
+        "length is a negative number"
+    }
+    else if (!object@n %% 1 == 0){
+        "length is not a integer"
+    }
+    else if(is.na(object@n)){
+        "length is not available"
+    }
+    else if (0>object@sd){
+        "Standard deviation is negative"
+    }
+    else if(length(object@data)!=object@n){
+        "length of data is not equal to noted length"
+    }
+    else if(!is.numeric(object@data)){
+        "Data is not numeric"
+    }
+    else if (any(is.na(object@data))){
+        "there are NA values in the data"
+    }
+    else{
+        
+        TRUE
+    }
+    #TODO laenge check und sd check
 }) 
 
 setValidity("AR", function(object) {
-    if (length(ar_params) != length(start_values)) {
+    if (length(object@ar_params) != length(object@start_values)) {
         "The number of starting values does not coincide with the amount of coefficients"
-    } else {
+    } 
+    else if(any(is.na(object@start_values))){
+        "there are NA start values"
+    }
+    
+    else if(any(is.na(object@ar_params))){
+        "there are NA AR Parameters"
+    }
+    else {
         TRUE
     }
 })
 
+setValidity("MA", function(object) {
+    if(any(is.na(object@ma_params))){
+        "there are NA MA-Parameters"
+    }
+    else {
+    TRUE
+      }
+})
 
-# Methode zur Generierung von AR(p) Daten
+# Resampling
 setGeneric(
     name = "resample",
     def = function(object) {
@@ -107,67 +187,8 @@ setMethod(
     }
 )
 
-# Beispielnutzung der S4-Klasse
-set.seed(123)
-n <- 10
-ar_params <- c(0.6, 0.6)  # AR(2)-Modell
-sd <- 1
-start_values = c(1,1)
-# Erstellen eines Objekts der ARTimeSeries-Klasse
-ar_time_series <- AR(start_values = start_values, ar_params = ar_params, sd = sd,n=10)
-
-# Plotten der AR(p)-Zeitreihe
-plot(ar_time_series@data)
-
-
-
-# Definition der S4-Klasse für MA(q) Zeitreihe
-setClass(
-    "MA",
-    slots = list(
-        ma_params = "numeric"
-        ),
-    contains="TimeSeries",
-    prototype = list(ma_params = NA_real_)
-    
-)
-
-# Benutzerdefinierter Konstruktor
-MA <- function(ma_params = NA_real_,sd=1,n=1) {
-    
-    ma_params <- ma_params
-    q <- length(ma_params)
-    
-    # Initialisieren der Zeitreihe mit Nullen
-    time_series <- numeric(n)
-    
-    # White-Noise-Komponente generieren
-    noise <- rnorm(n+q , 0, sd)
-    
-    
-    # Generieren der MA(q)-Zeitreihe
-    for (t in (q + 1):(n + q)) {
-        time_series[t] <- sum(ma_params * rev(time_series[(t-q):(t-1)])) + noise[t]
-    }
-    
-    new("MA",
-        ma_params=ma_params,
-        sd=sd,
-        data=time_series)
-}
-
-setValidity("MA", function(object) {
-   # if (length(ar_params) != length(start_values)) {
-        "The number of starting values does not coincide with the amount of coefficients"
-    #} else {
-        TRUE
-  #  }
-})
-
 
 # Methode zur Generierung von MA(q) Daten
-
-
 setMethod(
     "resample",
     "MA",
@@ -181,7 +202,7 @@ setMethod(
         
         # White-Noise-Komponente generieren
         noise <- rnorm(n+q , 0, object@sd)
-    
+        
         
         # Generieren der MA(q)-Zeitreihe
         for (t in (q + 1):(n + q)) {
@@ -192,22 +213,40 @@ setMethod(
     }
 )
 
-# Beispielnutzung der S4-Klasse
+
+
+
 set.seed(123)
 n <- 10
-ma_params <- c(0.6, 0.6)  # AR(2)-Modell
 sd <- 1
+# Beispielnutzung AR
+ar_params <- c(0.6, 0.6)  # AR(2)-Modell
+
+start_values = c(1,1)
+# Erstellen eines Objekts der ARTimeSeries-Klasse
+ar_time_series <- AR(start_values = start_values, ar_params = ar_params, sd = sd,n=10)
+
+# Plotten der AR(p)-Zeitreihe
+plot(ar_time_series@data)
+
+
+# Beispielnutzung MA
+ma_params <- c(0.6, 0.6)  # MA(2)-Modell
 # Erstellen eines Objekts der MATimeSeries-Klasse
 ma_time_series <- MA(ma_params = ma_params, sd = sd,n=n)
 
 # Plotten der MA(q)-Zeitreihe
 plot(ma_time_series@data)
 
-# auto covariance funktion (hier wird angenommen ts ist noch keine Klasse)
-ACF <- function(ts,h){
+#----------------------------
+# auto covariance funktion (hier wird angenommen ts_obj ist eine Klassenobjekt)
+ACF <- function(ts_obj,h){
+    #check validity of object again
+    ts <- ts_obj@data
+    n <- ts_obj@n
     stopifnot("Input is not of type numeric"=class(ts)=="numeric")
-    stopifnot("index out of bounds"=abs(h)<length(ts))
-    n <- length(ts)
+    stopifnot("index out of bounds"=abs(h)<ts_obj@n)
+    
     smpl_mean <- mean(ts)
     
     summe <-  (1/n) * (ts[(1+abs(h)):n]-smpl_mean) %*% (ts[1:(n-abs(h))]-smpl_mean)
@@ -216,5 +255,4 @@ ACF <- function(ts,h){
 
 
 
-ts <- ma_time_series
-plot(sapply(1:(length(ts)-1),function(h){ACF(ts,h)}))
+plot(sapply(1:(ma_time_series@n-1),function(h){ACF(ma_time_series,h)}))
