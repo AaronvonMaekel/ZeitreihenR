aed0d664069cb0a8f0f4d69c59410cbe665b332a
klausstier <118102996+klausstier@users.noreply.github.com>
2024-08-05 16:21:10 +0200

    Fixes in the innovations algortihm plus first implementation of a innvoations-based predicter

--- Innovations algorithm.R
+++ Innovations algorithm.R
@@ -4,18 +4,23 @@ innovations_algorithm <- function(ts_obj) {
     v <- rep(0,len)
     v[1] <- cov
     theta <- matrix(0,len,len)
+    acf_compl <- c( sapply(1:(len-1), \(x) {ACF(ts_obj, x)}), 0)
     for(n in 1:len) {
         if (n==1) {
-            theta[1][1] <- 1/v[1] * cov
-            v[2] <- cov - theta[1][1]^2 * v[1]
+            theta[1, 1] <- 1/v[1] * acf_compl[1]
+            v[2] <- cov - theta[1, 1]^2 * v[1]
         } else {
             for (k in 0:(n-1)) {
-                sum_thetas <- 0
-                for (j in 0:(k-1)) {
-                    sum_thetas <- sum_thetas + theta[k,k-j]*theta[n,n-j]*v[j+1]
+                if (k == 0){
+                    theta[n, n] <- acf_compl[n] / v[1]
+                } else {
+                    sum_thetas <- 0
+                    for (j in 0:(k-1)) {
+                        sum_thetas <- sum_thetas + theta[k,k-j]*theta[n,n-j]*v[j+1]
+                    }
+                    theta[n,n-k] <- 1/v[k+1] * (acf_compl[n-k] - sum_thetas)
                 }
-                covariance <- ACF(ts_obj,n-k)
-                theta[n,n-k] <- 1/v[k+1] * (covariance - sum_thetas)
+                
             }
             sum_v <- 0
             for(j in 0:(n-1)) {
@@ -26,3 +31,24 @@ innovations_algorithm <- function(ts_obj) {
     }
     return(theta)
 }
+
+
+innovations_algorithm(ar_time_series)
+
+# First implementation of a predicter for the innovations algorithm
+innovations_predict <- function (ts_obj, steps = 1){
+    thetas <- innovations_algorithm(ts_obj)
+    X_hat <- 0
+    len <- ts_obj@n
+    for (n in 1:len){
+        X <- ts_obj@data[1:n]
+        theta_calc <- thetas[n, ][1:n]
+        new_X <- sum(theta_calc * rev(X - X_hat))
+        X_hat <- c(X_hat, new_X)
+    }
+    return(X_hat[len+1])
+}
+
+
+innovations_predict(ar_time_series)
+plot(ar_time_series@data)
