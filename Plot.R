d0fafa7f9d5f324fefc051c612730ec331837014
vicitoria <114218283+vicitoria@users.noreply.github.com>
2024-08-09 15:58:29 +0200

    Update Plot.R
    Implemented plots for time series object and periodogram

--- Plot.R
+++ Plot.R
@@ -2,118 +2,104 @@ library(methods)
 library(ggplot2)
 library(tibble)
 
-# Anpassung der Plot-Funktion für S4-Zeitreihen-Objekte
-plot_timeseries <- function(ts_obj, prd = NULL, title = "Zeitreihe mit Vorhersage") {
-    # Überprüfen, ob das Eingabeobjekt eine valide Zeitreihe ist
+
+plot_timeseries <- function(ts_obj, prd = NULL, title = "Timeseries (with prediction)") {
+    # Check if ts_obj is a valid timeseries
     validObject(ts_obj)
     
-    # Extrahiere die Daten aus dem Zeitreihenobjekt
+    # Extract data
     timeseries <- ts_obj@data
     
-    # Eingabeüberprüfung
-    # stopifnot("Übergabegabevektor nicht numerisch." = is.numeric(timeseries))
-    # stopifnot("Übergabevektor nicht lang genug." = length(timeseries) > 0)
-    # stopifnot("Übergabevektor nicht numerisch oder NULL." = (is.numeric(prd) | is.null(prd)))
-    # Das auskommentierte sollte nicht notwendig sein, da wir das schon bei ValidObject abprüfen
-    
     if (is.null(prd)) {
-        # Plots erstellen
-        tibble2plot <- tibble::tibble(Wert = timeseries, Zeit = seq_along(timeseries))
-        plt_base <- ggplot2::ggplot(data = tibble2plot, mapping = ggplot2::aes(x = Zeit, y = Wert))
-        lay <- ggplot2::geom_line(color = "blue")
-        point <- ggplot2::geom_point(color = "red")
+        # Create plots
+        tb <- tibble::tibble(Value = timeseries, Time = seq_along(timeseries))
+        plt1 <- ggplot2::ggplot(data = tb, mapping = ggplot2::aes(x = Time, y = Value))
+        line <- ggplot2::geom_line(color = "purple")
+        jitter <- ggplot2::geom_jitter(color = "royalblue",shape=4)
+        layout <- ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5, size = 17))
         title <- ggplot2::ggtitle(title)
-        theme <- ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5, size = 15))
-        plt <- plt_base + lay + point + title + theme
+        plt <- plt1 + line + jitter + title + layout
         plt
     } else {
-        # Timeseries mit Vorhersage plotten
-        value_tbl <- tibble::tibble(Wert = timeseries, Zeit = seq_along(timeseries))
-        value_tbl$Group <- "Series"
+        # Check if prd is valid timeseries
+        validObject(prd)
+        # Create plots with forecasts
+        tb <- tibble::tibble(Value = timeseries, Time = seq_along(timeseries))
         
-        prd_tbl <- tibble::tibble(Wert = prd, Zeit = length(timeseries) + seq_along(prd))
-        prd_tbl$Group <- "Forecast"
+        prd_tbl <- tibble::tibble(Value = prd@data, Time = length(timeseries) + seq_along(prd@data))
         
-        tibble2plot <- rbind(value_tbl, prd_tbl)
-        
-        plt_base <- ggplot2::ggplot(data = tibble2plot, mapping = ggplot2::aes(x = Zeit, y = Wert))
-        lay1 <- ggplot2::geom_line(color = "#F8766D")
-        lay2 <- ggplot2::geom_line(ggplot2::aes(colour = factor(Group)))
-        point <- ggplot2::geom_point(data = tibble2plot, ggplot2::aes(x = Zeit, y = Wert))
-        labs <- ggplot2::labs(colour = "")
+        tb_combine <- rbind(tb, prd_tbl)
+    
+        plt2 <- ggplot2::ggplot(data = tb_combine, mapping = ggplot2::aes(x = Time, y = Value))
+        lay1 <- ggplot2::geom_line(color = "purple")
+        point <- ggplot2::geom_point(data = tb_combine[1:ts_obj@n,], shape=4, colour="royalblue")
+        point2 <- ggplot2::geom_point(data = tb_combine[(ts_obj@n+1):(ts_obj@n+prd@n),],shape=8,colour="green")
+        name <- ggplot2::labs(shape = "")
         title <- ggplot2::ggtitle(title)
-        theme <- ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5, size = 15))
-        plt <- plt_base + lay1 + lay2 + point + title + labs + theme
+        theme <- ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5, size = 17))
+        plt <- plt2 + lay1 + point + point2 + title + name+ theme
         plt
     }
+    
 }
 
-# Beispielnutzung mit AR-Zeitreihe
+# Example AR-timeseries
 n <- 100
 sd <- 1
 ar_params <- c(0.6, 0.6)
 start_values <- c(1, 1)
 
-# AR-Zeitreihe erzeugen
+# Create AR-timeseries
 ar_time_series <- AR(start_values = start_values, ar_params = ar_params, sd = sd, n = n)
 
-# Plotten der AR-Zeitreihe
+
+# Plot AR-timeseries
 plot_timeseries(ar_time_series, title = "AR Time Series Plot")
 
-# Beispiel mit Vorhersage
-prd <- rep(1, 10)  # Beispielhafte Vorhersage
-plot_timeseries(ar_time_series, title = "AR Time Series with Forecast")
+
+# Plot timeseries with prediction
+prd <- rep(1, 10) 
+prv <- vec_to_ts(prd)
+plot_timeseries(ar_time_series, title = "AR Time Series with Forecast",prd=prv)
 
 
-#Plotten des Periodogramms 
-plot_periodogram <- function(ts_obj, logscale = FALSE) {
-    # Überprüfen, ob das Eingabeobjekt eine valide Zeitreihe ist
+
+# Plot of periodogram
+plot_periodogram <- function(ts_obj) {
+    # Check if ts_obj is valid time series
     validObject(ts_obj)
     
-    #Extrahiere die Daten aus dem Zeitreihenobjekt
+    # Extract data from timeseries
     timeseries <- ts_obj@data
     
-    # Berechne das Periodogramm
-    periodogram <- periodogram_gen(ts_obj)
+    # Create periodogram
+    periodogram <- periodogram(ts_obj)
     
-    # Eingabeüberprüfung
-    stopifnot("Der Eingabevektor periodogram ist nicht numerisch." = is.numeric(periodogram))
-    stopifnot("Der Vektor periodogram muss wenigstens die Länge 1 haben." = length(periodogram) > 0)
-    
-    # Frequenzen generieren
+     # Frequenzen generieren
     n <- length(periodogram)
-    k <- floor((-(n-1)/2):(n/2))
+    k <- seq(-floor((n-1)/2),floor(n/2))
     freq <- 2 * pi * k / n
     
     # Periodogramm plotten
-    tibble2plot <- tibble::tibble(Wert = periodogram, Fourierfrequenz = freq)
-    plt_base <- ggplot2::ggplot(data = tibble2plot, mapping = ggplot2::aes(x = Fourierfrequenz, y = Wert))
-    lay <- ggplot2::geom_line(color = "#6a93b0")
-    point <- ggplot2::geom_point(color = "black")
-    labs <- ggplot2::ggtitle("Periodogramm der Zeitreihe")
-    
-    if (logscale) {
-        trans <- ggplot2::scale_y_continuous(trans = "log10")
-    } else {
-        trans <- NULL
-    }
-    
-    plt <- plt_base + lay + point + labs + trans + ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5, size = 15))
+    tibble2plot <- tibble::tibble(Value = periodogram, FourierFrequency = freq)
+    plt_base <- ggplot2::ggplot(data = tibble2plot, mapping = ggplot2::aes(x = FourierFrequency, y = Value))
+    lay <- ggplot2::geom_line(color = "purple")
+    point <- ggplot2::geom_point(color = "royalblue")
+    labs <- ggplot2::ggtitle("Timeseries Periodogram")
+    plt <- plt_base + lay + point + labs  + ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5, size = 15))
     plt
 }
 
-# Beispielnutzung mit einer AR-Zeitreihe
-set.seed(123)
+# Example
 n <- 100
 sd <- 1
 ar_params <- c(0.6, 0.6)
 start_values <- c(1, 1)
 
-# AR-Zeitreihe erzeugen
+# Create TimeSeries
 ar_time_series <- AR(start_values = start_values, ar_params = ar_params, sd = sd, n = n)
 
-# Periodogramm plotten
-plot_periodogram(ar_time_series, logscale = FALSE)
+# Plot periodogram
+plot_periodogram(ar_time_series)
 
-# Periodogramm im Log-Scale plotten
-plot_periodogram(ar_time_series, logscale = TRUE)
+
