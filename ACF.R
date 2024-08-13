6d168e247689d1e441c3407ae3d46de6554599ca
vicitoria <114218283+vicitoria@users.noreply.github.com>
2024-08-13 17:12:50 +0200

    Update ACF.R
    tidying

--- ACF.R
+++ ACF.R
@@ -1,15 +1,15 @@
 #'Sample autocovariance function
 #'
-#'@description Takes a TimeSeries object and computes the sample autocovariance function of the data.
+#'@description This function takes a \code{TimeSeries} object and computes the sample autocovariance function of the data.
 #'
-#'@param ts_obj A time series, must be a TimeSeries class.
-#'@param h Point for which we want the function value of the sample autocovariance function.
+#'@param ts_obj A time series, which must be a \code{TimeSeries} class.
+#'@param h Point for which we want the function value of the sample autocovariance function, must be a length one numeric vector.
 #'
-#'@return The return value is a numeric value which can be regarded as an estimation of the autocovariance function.
+#'@return The return value is a numeric value which can be regarded as estimation of the autocovariance function.
 #'
 #'@examples 
 #'ar_ts <- AR(ar_params = 0.5, start_values = 1, n = 50, sd = 1)
-#'AcF(ar_ts,2)
+#'ACF(ar_ts,2)
 #'
 #'@export
 
@@ -31,6 +31,4 @@ setMethod("ACF",
               summe <-  (1/n) * (ts[(1+abs(h)):n]-smpl_mean) %*% (ts[1:(n-abs(h))]-smpl_mean)
               return(summe[1][1])
           })
-
-plot(sapply(1:(ma_time_series@n-1),function(h){ACF(ma_time_series,h)}))
 
