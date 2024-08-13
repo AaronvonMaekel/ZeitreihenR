261ac5d2945c63b0b27526c3b7a580b3c9b19684
julia-127 <ju.renner@outlook.de>
2024-08-13 16:32:33 +0200

    roxygen2 comments added, periodogram as generic function

--- PeriodogramChecked.R
+++ PeriodogramChecked.R
@@ -1,34 +1,48 @@
-periodogram <- function(ts_obj){
-    # Validity
-    validObject(ts_obj)
-    
-    data <- ts_obj@data
-    len <- ts_obj@n
-    
-    # Timeseries must contain more than one element
-    stopifnot("Die Zeitreihe muss mehr als ein Element enthalten."=len>1)
+#'Periodogram for a time series
+#'
+#'@description Takes a TimeSeries object and computes the periodogram of the time series.
+#'
+#'@param ts_obj A time series, must be a TimeSeries class.
+#'
+#'@return The return value is a list with values for the frequency and the spectral density.
+#'
+#'@examples 
+#'ar_time_series <- AR(start_values = c(1, 1), ar_params = c(0.6, 0.6), sd = 1, n = 100)
+#'periodogram(ar_time_series)
+#'
+#'@export
 
-    # Initializing periodogram 
-    spec <- numeric(len)
-    
-    # Frequency index
-    freq_index <- seq(-floor((len-1)/2),floor(len/2))
-    
-    # Calculating the periodogram
-    i <- 0
-    for (k in freq_index){
-        summation <- sum(data*exp(-1i*2*pi*k*(1:len)/len))
-        spec[i] <- (1/len)*abs(summation)^2
-        i <- i+1
-    }
-    
-    
-    freq <- freq_index * 2 * pi/len
+setGeneric("periodogram", 
+           function(ts_obj) standardGeneric("periodogram"))
 
-    # Return as list
-    return(list(freq=freq,density=spec))
-}
-
-periodogram(ar_time_series)
-
-
+setMethod("periodogram",
+          "TimeSeries",
+          function(ts_obj){
+            # Validity
+            validObject(ts_obj)
+            
+            data <- ts_obj@data
+            len <- ts_obj@n
+            
+            # Timeseries must contain more than one element
+            stopifnot("Die Zeitreihe muss mehr als ein Element enthalten."=len>1)
+            
+            # Initializing periodogram 
+            spec <- numeric(len)
+            
+            # Frequency index
+            freq_index <- seq(-floor((len-1)/2),floor(len/2))
+            
+            # Calculating the periodogram
+            i <- 0
+            for (k in freq_index){
+                summation <- sum(data*exp(-1i*2*pi*k*(1:len)/len))
+                spec[i] <- (1/len)*abs(summation)^2
+                i <- i+1
+            }
+            
+            freq <- freq_index * 2 * pi/len
+            
+            # Return as list
+            return(list(freq=freq,density=spec))
+        })
