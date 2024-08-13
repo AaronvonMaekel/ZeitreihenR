55afa1cc37ecbdb306cff3d1662dfe5d8852e9ed
klausstier <118102996+klausstier@users.noreply.github.com>
2024-08-13 10:27:30 +0200

    Added comments to improve readibility and understanding of the code

--- durbin levinson.R
+++ durbin levinson.R
@@ -1,16 +1,24 @@
+# Durbin-Levinson Algorithm function
+
 dla <- function(ts_obj){
+    # Save number of values in timeseries
     max_n <- ts_obj@n
+    
+    # Computing the ACF values
     acf_compl <- c( sapply(1:(max_n-1), \(x) {ACF(ts_obj, x)}), 0)
+    
+    # Compute coefficients
     for (n in 1:max_n) {
         if (n == 1) { 
+            # Initialization
             phi <- ACF(ts_obj, 1) / ACF(ts_obj, 0)
             phi_n <- phi
             nu <- ACF(ts_obj, 0) * (1 - phi^2)
         } else {
-            # dla_step(ts_obj, j, phi, ) NOTE: Here we could (alternatively) always call a "step" function
-            # However, I think that's quite difficult, as we need to return both Nu and Phi
+            # Recursive computation
             val <- sapply(1:(n-1), \(j) {ACF(ts_obj, n-j) * phi[j]})
-            #print(val)
+            
+            # Update values of phi and nu
             phi_n <- (1/nu)*(acf_compl[n] - sum(val))
             phi <- phi - phi_n * rev(phi)
             phi <- c(phi, phi_n)
@@ -21,22 +29,10 @@ dla <- function(ts_obj){
 }
 
 
-
-dla(ar_time_series)
-
-
-# First testing
-ss <- dla(ar_time_series)
-ne <- sum(ss * rev(ar_time_series@data))
-ar_time_series@n <- ar_time_series@n + 1
-ar_time_series@data <- c(ar_time_series@data, ne)
-plot(ar_time_series@data)
-
-ar_time_series@n
-ar_time_series@data
-
-
+# Predictor based on the DL algorithm
 dl_predictor <- function(ts_obj, pred_len=1, entire_ts = TRUE){
+    
+    # Checking validity
     stopifnot("Prediction length should be greater or equal to 1" =  pred_len >= 1)
     stopifnot("Entered preditcion length not compatible" = length(pred_len) == 1)
     stopifnot("Prediction length is not an integer" = pred_len %% 1 == 0)
@@ -46,12 +42,16 @@ dl_predictor <- function(ts_obj, pred_len=1, entire_ts = TRUE){
     
     in_len <- ts_obj@n
     for (j in 1:pred_len){
+        # Computing estimate using coefficients obtained from the DL algorithm
         new_val <- sum(dla(ts_obj) * rev(ts_obj@data))
+        
+        # Appending estimated value to the time series (needed for further estimates)
         ts_obj@data <- c(ts_obj@data, new_val)
         ts_obj@n <- ts_obj@n + 1
     }
     vec <- ts_obj@data
 
+    # Producing output according to the specification made by entire_ts
     if (entire_ts==TRUE){
         return(vec_to_ts(vec))
     } else {
