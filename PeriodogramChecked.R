30587acf5e00dee36cbeb8ce6639bcd7f692bd5d
vicitoria <114218283+vicitoria@users.noreply.github.com>
2024-08-13 15:56:28 +0200

    Update PeriodogramChecked.R
    Change language

--- PeriodogramChecked.R
+++ PeriodogramChecked.R
@@ -1,27 +1,31 @@
 periodogram <- function(ts_obj){
-    #Validation
+    # Validity
     validObject(ts_obj)
     
     data <- ts_obj@data
     len <- ts_obj@n
-    #Zeitreihe ausreichend lang
+    
+    # Timeseries must contain more than one element
     stopifnot("Die Zeitreihe muss mehr als ein Element enthalten."=len>1)
 
-    #Periodogrammspeicher
+    # Initializing periodogram 
     spec <- numeric(len)
-    #Frequenzbereich
+    
+    # Frequency index
     freq_index <- seq(-floor((len-1)/2),floor(len/2))
     
-    #Berechnung Periodogramm
+    # Calculating the periodogram
     i <- 0
     for (k in freq_index){
         summation <- sum(data*exp(-1i*2*pi*k*(1:len)/len))
         spec[i] <- (1/len)*abs(summation)^2
         i <- i+1
     }
-    #Rückgabe als Liste
+    
     
     freq <- freq_index * 2 * pi/len
+
+    # Return as list
     return(list(freq=freq,density=spec))
 }
 
