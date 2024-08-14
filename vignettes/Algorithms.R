1b65925e163c8541049cce04c46b9f22da7c2bf5
vicitoria <114218283+vicitoria@users.noreply.github.com>
2024-08-14 15:42:58 +0200

    Create Algorithms.R

---
+++ vignettes/Algorithms.R
@@ -0,0 +1,21 @@
+---
+title: "Algorithms"
+output: rmarkdown::html_vignette
+vignette: >
+  %\VignetteIndexEntry{Algorithmss}
+  %\VignetteEngine{knitr::rmarkdown}
+  %\VignetteEncoding{UTF-8}
+---
+
+```{r, include = FALSE}
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+```
+
+```{r setup}
+library(ZeitreihenR)
+library(ggplot2)
+library(tibble)
+```
